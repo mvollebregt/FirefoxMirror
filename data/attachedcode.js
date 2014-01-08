a1d7eb76bb6cb59252f0ec4ba1c95caf02a9e0aa
Michel Vollebregt <mvollebregt@xs4all.nl>
2014-01-08 12:39:45 +0100

    Attached a script to the document’s DOM that dumps the document’s HTML into a javascript alert

---
+++ data/attachedcode.js
@@ -0,0 +1,10 @@
+/**
+ * Code in this file will be attached to the DOM of the page.
+ */
+
+var html = document.documentElement.innerHTML;
+
+
+
+// alert(document.documentElement.innerHTML); 
+// document.body.style.border = "5px solid red";
